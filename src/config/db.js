556a832f563fa20dc46286df3deec0a5a5b88d51
Phyo Hein ( Ezekiel ) <mwrcps11@gmail.com>
2026-01-12 13:31:37 +0700

    Hosting on Render

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
-console.log(process.env.DB_NAME);
 const mongoose = require('mongoose');
-const password = encodeURIComponent(process.env.PASSWORD);
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL_FRONT + password + process.env.MONGO_URL_END + process.env.DB_NAME);
+        await mongoose.connect(process.env.MONGO_URL);
+
+
         console.log('MongoDB connected');
 
     } catch (error) {
